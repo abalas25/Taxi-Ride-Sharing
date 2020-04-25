6ae927898080b5d62b156242735094fe5eed46b6
siddhanth98 <55416221+siddhanth98@users.noreply.github.com>
2020-04-25 18:42:42 -0500

    Delete init_db.py

--- init_db.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-from postgis.psycopg import register
-
-
-conn = psycopg2.connect(host='******', user='******', password='******', port='****', dbname='******')
-register(conn)
-curr = conn.cursor()
